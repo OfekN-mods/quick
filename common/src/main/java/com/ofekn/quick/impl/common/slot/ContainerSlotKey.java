8961e38abba4b6a51e735ca58a8572a74d4951bf
OfekN_ <ofek2608@gmail.com>
2026-05-25 14:43:05 +0300

    added slot modification check

--- common/src/main/java/com/ofekn/quick/impl/common/slot/ContainerSlotKey.java
+++ common/src/main/java/com/ofekn/quick/impl/common/slot/ContainerSlotKey.java
@@ -6,7 +6,10 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.Optional;
 
 public record ContainerSlotKey(int index) implements ISlotKey {
     public static final StreamCodec<ByteBuf, ContainerSlotKey> STREAM_CODEC = StreamCodec.composite(
@@ -28,23 +31,29 @@ public record ContainerSlotKey(int index) implements ISlotKey {
 
     @Override
     public ItemStack get(Player player) {
-        var slots = player.containerMenu.slots;
-        return index < slots.size() ? slots.get(index).getItem() : ItemStack.EMPTY;
+        return getSlot(player).map(Slot::getItem).orElse(ItemStack.EMPTY);
     }
 
     @Override
     public boolean set(Player player, ItemStack stack) {
-        var slots = player.containerMenu.slots;
-        if (index >= slots.size()) {
+        var slotOpt = getSlot(player).filter(slot -> slot.allowModification(player));
+        if (slotOpt.isEmpty()) {
             return false;
         }
-        var slot = slots.get(index);
-        slot.set(stack);
-        return slot.getItem() == stack; // check just in case
+        slotOpt.get().set(stack);
+        return slotOpt.get().getItem() == stack; // check just in case
+    }
+
+    private Optional<Slot> getSlot(Player player) {
+        var slots = player.containerMenu.slots;
+        if (index > slots.size()) {
+            return Optional.empty();
+        }
+        return Optional.of(slots.get(index));
     }
 
     @Override
     public boolean isModifiable(Player player) {
-        return true;
+        return getSlot(player).map(slot -> slot.allowModification(player)).orElse(false);
     }
 }
