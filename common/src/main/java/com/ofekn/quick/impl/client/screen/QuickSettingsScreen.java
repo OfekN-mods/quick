397672f8e2b6753cddd9562687d9b3cf83bbe07a
OfekN_ <ofek2608@gmail.com>
2026-05-30 04:10:32 +0300

    action buttons

--- common/src/main/java/com/ofekn/quick/impl/client/screen/QuickSettingsScreen.java
+++ common/src/main/java/com/ofekn/quick/impl/client/screen/QuickSettingsScreen.java
@@ -2,19 +2,17 @@ package com.ofekn.quick.impl.client.screen;
 
 import com.ofekn.quick.api.client.QuickClientRegistry;
 import com.ofekn.quick.api.client.WheelLayout;
+import com.ofekn.quick.impl.client.QuickKeyMappings;
 import com.ofekn.quick.impl.common.integration.QuickIntegrations;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
-import org.jspecify.annotations.Nullable;
 
 import java.util.Objects;
 
 public class QuickSettingsScreen extends Screen {
-    @Nullable
-    private Button button;
 
     public QuickSettingsScreen() {
         super(Component.translatable("gui.quick.settings"));
@@ -28,11 +26,18 @@ public class QuickSettingsScreen extends Screen {
     @Override
     protected void init() {
         super.init();
-        button = addRenderableWidget(Button.builder(Component.empty(), this::toggleLayout).pos(8, 8).build());
-        updateToggleLayoutText();
+        addRenderableWidget(Button.builder(getToggleLayoutButtonText(), QuickSettingsScreen::toggleLayout).pos(8, 8).build());
+        var assignedActions = QuickKeyMappings.get().assignedActions;
+        for (int i = 0; i < assignedActions.size(); i++) {
+            String keybindName = assignedActions.get(i).getName();
+            Component keybindComponent = Component.keybind(keybindName);
+            Component buttonComponent = Component.translatable("gui.quick.settings.set_action", keybindComponent);
+            final int finalI = i;
+            addRenderableWidget(Button.builder(buttonComponent, _ -> editAction(finalI)).pos(8, 8 + 28 + 28 * i).build());
+        }
     }
 
-    private void toggleLayout(Button button) {
+    private static void toggleLayout(Button button) {
         String layoutStr = QuickIntegrations.CONFIG.getWheelType();
         Identifier layoutId = Identifier.tryParse(layoutStr);
         WheelLayout layout = layoutId == null ? null : QuickClientRegistry.WHEEL_LAYOUT.getValue(layoutId);
@@ -46,14 +51,15 @@ public class QuickSettingsScreen extends Screen {
         Identifier newLayoutId = QuickClientRegistry.WHEEL_LAYOUT.getKey(newLayout);
         Objects.requireNonNull(newLayoutId);
         QuickIntegrations.CONFIG.setWheelType(newLayoutId.toString());
-        updateToggleLayoutText();
+        button.setMessage(getToggleLayoutButtonText());
     }
 
-    private void updateToggleLayoutText() {
-        if (button == null) {
-            return;
-        }
+    private static void editAction(int index) {
+        // TODO implement
+    }
+
+    private static Component getToggleLayoutButtonText() {
         Component layout = Component.literal(QuickIntegrations.CONFIG.getWheelType()).withStyle(ChatFormatting.AQUA);
-        button.setMessage(Component.translatable("gui.quick.settings.toggle_layout", layout));
+        return Component.translatable("gui.quick.settings.toggle_layout", layout);
     }
 }
